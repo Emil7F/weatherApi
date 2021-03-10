bf1287a1be6652725c8f4508ac93a4ec69334d52
Emil <emilfigzal@gmail.com>
2021-03-10 21:24:20 +0100

    connection to weather api

--- src/main/java/pl/emil7f/weatherapi/service/WeatherService.java
+++ src/main/java/pl/emil7f/weatherapi/service/WeatherService.java
@@ -1,13 +1,29 @@
 package pl.emil7f.weatherapi.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import pl.emil7f.weatherapi.model.WeatherDto;
 
 @Service
 public class WeatherService {
 
-    public WeatherDto getWeather(){
-        throw new NullPointerException("implement!");
+    public static final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/";
+    public static final String API_KEY = "cbadd1e2fac6a5084c2c34b65afbe179";
 
+    private  RestTemplate restTemplate = new RestTemplate();
+
+
+
+
+    public WeatherDto getWeather() {
+        String forObject = getWeatherForCity("porto");
+        System.out.println(forObject);
+        return null;
+    }
+
+
+    private String getWeatherForCity(String city ) {
+        return restTemplate.getForObject(WEATHER_URL + "weather?q={city}&appid={apikey}&units=metric&lang=pl",
+                String.class, city, API_KEY);
     }
 }
